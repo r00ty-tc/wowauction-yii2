96510d3468cb05e182f3d50df074b0e59212b22d
r00ty-tc <r00ty-tc@users.noreply.github.com>
2023-11-12 15:45:08 +0000

    Updated Discord bot page to cover new channel specific commands

--- views/site/discord.php
+++ views/site/discord.php
@@ -10,6 +10,10 @@
     <li>type <pre>/ahsetrealm < realm_name ></pre></li> This realm_name is the name of realm.
     <li>then type <pre>/ahsetfaction < faction ></pre> faction can be <b>a</b>(alliance) or <b>h</b>(horde)</li>
     <li>then you are all set! <pre>/ahsearch < item name ></pre> item name is any item that you are looking for eg : pillars of might, cardinal ruby</li>
+    <br>For more advanced settings, it's possible to adjust faction and/or realm per channel. The server default is overridden by this setting only in the channels it is configured in</br>
+    <b>In the channel you want to configure:</b>
+    <li>type <pre>/ahsetchannelrealm < realm_name ></pre> The realm name is the same as the server wide realm setting command</li>
+    <li>type <pre>/ahsetchanneldfaction < faction ></pre> The faction is the same as the server wide faction setting command</li>
 </ol>
 <h2>Example</h2><br>
 <img src="/images/discord_example.png">
